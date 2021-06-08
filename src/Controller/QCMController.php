6483c03d7c4e244418f3a672076140f46a273d7e
yosri5031 <saloumayos88999@gmail.com>
2021-06-08 11:55:22 +0100

    ADD QCM PAGE

---
+++ src/Controller/QCMController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class QCMController extends AbstractController
+{
+    /**
+     * @Route("/qcm", name="qcm")
+     */
+    public function index(): Response
+    {
+        return $this->render('pages/qcm.html.twig');
+    }
+}
